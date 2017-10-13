7bbdad358e30400ae3237488cd0a8386bbbbee1f
charleypaulus <charleypaulus@hotmail.com>
2017-10-13 10:48:00 -0400

    Compute rankings

--- AssisesAnalytics.py
+++ AssisesAnalytics.py
@@ -5,14 +5,35 @@ import urllib2
 sitemap_file = "url_list.xml"
 site = etree.parse(sitemap_file).getroot()
 
-globalCounter = 0
-categoryCounter = 0
-questionCounter = 0
-contributionCounter = 0
+numberContributionGlobal = 1
+numberContributionCategory = 1
+numberContributionSubcategory = 1
+selfRankGlobal = 1
+selfRankCategory = 1
+selfRankSubcategory = 1
+selfExaequoGlobal = 0
+selfExaequoCategory = 0
+selfExaequoSubcategory = 0
 selfCategory = False
-selfQuestion = False
+selfSubcategory = False
 
 
+# Get self contribution score
+subcategory = site.xpath('//subcategory[@self="true"]')[0]
+url = subcategory.get('url')
+id = subcategory.get('self-contrib-id')
+
+page = html.fromstring(urllib2.urlopen(subcategory.get("url")).read())
+contribution = page.xpath('//div[@class = "user-contribution block-content" and @data-contrib = "' + id + '"]')[0]
+plusNumber = int(contribution.xpath('.//div[@class="float-left like-count-entity-node"]')[0].text)
+#minusNumber = contribution.xpath('.//div[@class="float-left dislike-count-entity-node"]')[0].text
+
+selfScore = plusNumber
+print "Score of R&D Hyperloop:", selfScore
+print ""
+
+
+#Scan all contribution
 for category in site:
 	if category.get("self") == "true":
 		selfCategory = True
@@ -21,25 +42,43 @@ for category in site:
 	
 	print 'Scanning category:', category.get("label")
 	
-	for question in category:
-		if question.get("self") == "true":
-			selfQuestion = True
+	for subcategory in category:
+		if subcategory.get("self") == "true":
+			selfSubcategory = True
 		else:
-			selfQuestion = False
+			selfSubcategory = False
 		
-		page = html.fromstring(urllib2.urlopen(question.get("url")).read())
-		page
-		for contribution in page.xpath('//div[@class="user-contribution block-content"]'):
-			contributionCounter += 1
-			plusNumber = contribution.xpath('.//div[@class="float-left like-count-entity-node"]')[0].text
-			minusNumber = contribution.xpath('.//div[@class="float-left dislike-count-entity-node"]')[0].text
+		page = html.fromstring(urllib2.urlopen(subcategory.get("url")).read())
+		for contribution in page.xpath('//div[@class = "user-contribution block-content" and @data-contrib != "' + id + '"]'):
+			numberContributionGlobal += 1
+			if selfCategory:
+				numberContributionCategory += 1
+			if selfSubcategory:
+				numberContributionSubcategory += 1
+			
+			plusNumber = int(contribution.xpath('.//div[@class="float-left like-count-entity-node"]')[0].text)
+			#minusNumber = int(contribution.xpath('.//div[@class="float-left dislike-count-entity-node"]')[0].text)
+			
+			score = plusNumber
+			print score
+			
+			if score > selfScore:
+				selfRankGlobal +=1
+				if selfCategory:
+					selfRankCategory += 1
+				if selfSubcategory:
+					selfRankSubcategory += 1
+			elif score == selfScore:
+				selfExaequoGlobal +=1
+				if selfCategory:
+					selfExaequoCategory += 1
+				if selfSubcategory:
+					selfExaequoSubcategory += 1
 		
 print ""		
-print 'Total contributions:', contributionCounter
-print ""
 print 'Ranking of "R&D Hyperloop" topic:'
-print '- In its sub-category: ', questionCounter
-print '- In its category: ', categoryCounter
-print "- In the whole site: ", globalCounter
+print '- In its sub-category: ', selfRankSubcategory, '/', numberContributionSubcategory, '( Ex aequo:', selfExaequoSubcategory, ')'
+print '- In its category: ', selfRankCategory, '/', numberContributionCategory, '( Ex aequo:', selfExaequoCategory, ')'
+print "- In the whole site: ", selfRankGlobal, '/', numberContributionGlobal, '( Ex aequo:', selfExaequoGlobal, ')'
 print ""
 raw_input("Press Enter to exit...")
